f91e5b8b99156b7621c09098c4ba0dd704f9e41c
Gyges <l2241883@gmail.com>
2018-07-13 15:10:53 +0800

    add java doc

--- src/main/java/com/unistack/tamboo/message/kafka/storage/OffsetBackingStore.java
+++ src/main/java/com/unistack/tamboo/message/kafka/storage/OffsetBackingStore.java
@@ -8,11 +8,19 @@ import java.util.List;
 /**
  * @author Gyges Zean
  * @date 2018/4/18
- * storage for topic offset in sqlite
+ * storage of topic offset
  */
 public interface OffsetBackingStore {
 
+    /**
+     * 获取所有group下所有consumer的消息消费信息
+     *
+     * @return
+     */
     List<ConsumerOffset> getConsumerGroups();
 
+    /**
+     * adminClient连接关闭
+     */
     void close();
 }
